7841b55ddbe69efd6039698db3bc982108a533c3
agaa-mac <agaa.mac@gmail.com>
2023-02-21 21:34:19 +0100

    Added links to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <footer>
+          This project was coded by Agata Mączyńska and is open-sourced on
+          <a
+            href="https://github.com/agaa-mac/react-dictionary-project"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          and hosted on
+          <a
+            href="https://dictionary-project-21022023.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
