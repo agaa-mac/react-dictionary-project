60177ab345e216f44b05dd216a94f9535cf8e83f
agaa-mac <agaa.mac@gmail.com>
2023-02-24 22:21:17 +0100

    Phonetics is working

--- src/Phonetics.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Phonetics(props) {
-  return "Hello from phonetics";
-}
--- src/Results.js
+++ src/Results.js
@@ -1,19 +1,12 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetics from "./Phonetics";
+
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-
-        {props.results.phonetic.map(function (phonetic, index) {
-          return (
-            <div key={index}>
-              <Phonetics phonetics={phonetic} />
-            </div>
-          );
-        })}
+        <h4>{props.results.phonetic} </h4>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
