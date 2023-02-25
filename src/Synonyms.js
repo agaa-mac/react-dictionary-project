2059036a1d6f1c47329e45e4ff72e6c800cff2d8
agaa-mac <agaa.mac@gmail.com>
2023-02-25 14:37:04 +0100

    Components: Synonyms and Meaning are styled and responsive on mobile devices

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
+        <li className="synonym">Synonyms:</li>
         {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
+          return <li key={index}>{synonym},</li>;
         })}
       </ul>
     );
