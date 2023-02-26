b6b3a4c9e21bdd5f9554e0ba1c9c0183a378771c
agaa-mac <agaa.mac@gmail.com>
2023-02-26 17:47:22 +0100

    fixed Result.css

--- src/Results.js
+++ src/Results.js
@@ -6,14 +6,14 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <section>
+        <section className="sectionResults">
           <h2>{props.results.word}</h2>
           <h4>{props.results.phonetic} </h4>
         </section>
 
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <section key={index}>
+            <section className="sectionResults" key={index}>
               <Meaning meaning={meaning} />
             </section>
           );
